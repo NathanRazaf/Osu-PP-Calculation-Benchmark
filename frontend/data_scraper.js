da1bfd91670e9a1a8309b3de31a3cee214ebbd01
NathanRazaf <natrazaf2020@gmail.com>
2024-11-24 19:31:38 -0500

    initial commit

--- frontend/data_scraper.js
+++ frontend/data_scraper.js
@@ -13,3 +13,15 @@ axios.get('http://localhost:3000/fetch/country/best/MG/2').then((response) =>
 )
 .catch((error) => {console.log(error);});
 
+async function scrapeData(countryCode, page) {
+    try {
+        const response = await axios.get(`http://localhost:3000/fetch/country/best/${countryCode}/${page}`);
+        fs.writeFileSync(
+            'data.json',
+            JSON.stringify(response.data, null, 2),
+            'utf-8'
+        );
+    } catch (error) {
+        console.log(error);
+    }
+}
